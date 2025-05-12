f9a150d7490273c3ed3c90284435a3ced46aca02
Thiago <thiagoalvez451@gmail.com>
2025-05-12 00:23:12 -0300

    Meu Projeto StarBucks

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".starbucks")
+
+
+function trocarCor(cor){
+   circulo.style.background = cor
+}
+
+function trocaImagem(img){
+    imagem.src = img
+}
